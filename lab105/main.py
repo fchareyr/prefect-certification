d0357ab60bdf66e91741fc41fb34f1a3f8ef8c3f
François Chareyron <francois.chareyron@me.com>
2024-12-18 15:47:20 +0100

    Add run_deployment

--- lab105/main.py
+++ lab105/main.py
@@ -1,4 +1,5 @@
 from prefect import task, flow
+from prefect.deployments import run_deployment
 
 @task
 def add(a: int, b: int) -> int:
@@ -12,11 +13,25 @@ def make_add():
 def make_operation():
     make_add()
 
+@flow
+def run_operation():
+    run_deployment(
+        name="make_operation/deploy-5-nested"
+    )
+
 if __name__ == '__main__':
     flow.from_source(
         source="https://github.com/fchareyr/prefect-certification.git",
         entrypoint="lab105/main.py:make_operation",
     ).deploy(
-        name="deploy-5",
+        name="deploy-5-nested",
+        work_pool_name="managed"
+    )
+
+    flow.from_source(
+        source="https://github.com/fchareyr/prefect-certification.git",
+        entrypoint="lab105/main.py:run_operation",
+    ).deploy(
+        name="deploy-5-run-deployment",
         work_pool_name="managed"
     )
